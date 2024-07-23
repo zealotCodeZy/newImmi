bbc5da822da5dca3a9dc627c0799163f614d1827
zealotCodeZy <461703952@qq.com>
2024-07-23 18:05:40 -0500

    add functions

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, abort
 from . import utils
 
 bp = Blueprint('main', __name__)
@@ -10,5 +10,17 @@ def index():
 @bp.route('/result', methods=['POST'])
 def result():
     zipcode = request.form['zipcode']
-    info = utils.get_info_by_zipcode(zipcode)
-    return render_template('result.html', info=info)
+    addresses = utils.get_addresses_by_zipcode(zipcode)
+    return render_template('result.html', zipcode=zipcode, addresses=addresses)
+
+@bp.route('/detail/<address>')
+def detail(address):
+    info = utils.get_info_by_address(address)
+    if info:
+        return render_template('detail.html', info=info)
+    else:
+        abort(404)
+
+@bp.route('/contact')
+def about():
+    return render_template('contact.html')
--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,22 @@
-def get_info_by_zipcode(zipcode):
-    # 模拟获取信息
-    info = {
-        'text': f"Information for ZIP code {zipcode}",
-        'image_url': 'https://via.placeholder.com/150'
-    }
-    return info
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('rentBlacklist.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_addresses_by_zipcode(zipcode):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT address FROM zipcode_info WHERE zipcode = ?", (zipcode,))
+    addresses = [row['address'] for row in cursor.fetchall()]
+    conn.close()
+    return addresses
+
+def get_info_by_address(address):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT zipcode, address, content FROM zipcode_info WHERE address = ?", (address,))
+    info = cursor.fetchone()
+    conn.close()
+    return dict(info) if info else None
